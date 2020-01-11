db650f0a97d59fd4984ca4abbed972004a0720b6
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2020-01-11 18:29:03 -0500

    test upload is deleted on completion of test

--- src/ResizeService.js
+++ src/ResizeService.js
@@ -1,3 +1,9 @@
+/******************************************************************************
+ * Written by Peter Craftsmn                                                  *
+ * Email: peter.craftsmn@gmail.com                                            *
+ * All rights reserved Copyright (c) 2020.                                    *
+ ******************************************************************************/
+
 class ResizeService {
 
 }

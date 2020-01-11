09f695a3cc02904e6067b6145d7a6b7723596818
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2020-01-11 17:36:57 -0500

    Upload Service refactored

--- src/UploadService.js
+++ src/UploadService.js
@@ -2,22 +2,28 @@ const multer = require( 'multer' );
 
 class UploadService {
     constructor() {
+        this.storage = multer.memoryStorage();
+
+        this.fileSize = { fileSize: 6 * 1024 * 1024 };
+
+        this.fileFilter = function fileFilter( req, file, cb ) {
+            let type = file.mimetype;
+            let typeArray = type.split( "/" );
+            if ( typeArray[ 0 ] === "image" &&
+                ( typeArray[ 1 ] === "png" ||
+                    typeArray[ 1 ] === "jpg" ||
+                    typeArray[ 1 ] === "jpeg"
+                ) ) {
+                cb( null, true );
+            } else {
+                cb( null, false );
+            }
+        };
+
         this.upload = multer( {
-            storage: multer.memoryStorage(),
-            limits: { fileSize: 6 * 1024 * 1024 },
-            fileFilter: function fileFilter( req, file, cb ) {
-                let type = file.mimetype;
-                let typeArray = type.split( "/" );
-                if ( typeArray[ 0 ] === "image" &&
-                    ( typeArray[ 1 ] === "png" ||
-                        typeArray[ 1 ] === "jpg" ||
-                        typeArray[ 1 ] === "jpeg"
-                    ) ) {
-                    cb( null, true );
-                } else {
-                    cb( null, false );
-                }
-            },
+            storage: this.storage,
+            limits: this.fileSize,
+            fileFilter: this.fileFilter,
         } );
     }
 }
